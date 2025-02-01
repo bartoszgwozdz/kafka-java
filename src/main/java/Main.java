88485d26ddef28f523b5f2527a05577bbbec6e68
bgwozdz <bartoszgwozdz@gmail.com>
2025-02-01 09:38:24 +0100

    Simple hardcoded response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Optional;
 
 public class Main {
   public static void main(String[] args){
@@ -19,6 +20,24 @@ public class Main {
        serverSocket.setReuseAddress(true);
        // Wait for connection from client.
        clientSocket = serverSocket.accept();
+       Optional<String> correlationId;
+//       try(BufferedInputStream inputStreamReader = new BufferedInputStream(clientSocket.getInputStream())){
+//           byte[] buffer = inputStreamReader.readAllBytes();
+//           String output = new String(buffer);
+//            correlationId = output.lines()
+//                   .filter(s -> s.startsWith("correlation_id"))
+//                   .map(s -> s.split(":")[1])
+//                   .findFirst();
+//
+//       }
+         OutputStream outputStream = clientSocket.getOutputStream();
+         outputStream.write(new byte[]{0,1,2,3});
+         outputStream.write(new byte[]{0,0,0,7});
+         outputStream.flush();
+//       String response = "message_size => INT32 \n" +
+//               "Response Header v0 => correlation_id \n" +
+//               "  correlation_id => 7";
+//       outputStream.write(response.getBytes());
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      } finally {
